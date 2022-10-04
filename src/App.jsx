ae3afd6382b3b85a32542a579b84aec7fd6ab291
zion livingstone <zionlivingstone@gmail.com>
2022-10-04 15:00:40 +0100

    new repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Register from './Register';
+import Login from './Login';
+import './index.css'
+
+function App() {
+
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+
+export default App;
